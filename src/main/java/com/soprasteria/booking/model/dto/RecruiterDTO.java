2f4bd8e57cf97623530128b2d36a1c6705674868
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-04-06 09:39:14 +0200

    feat: attributes added to DTO RecruiterDTO

--- src/main/java/com/soprasteria/booking/model/dto/RecruiterDTO.java
+++ src/main/java/com/soprasteria/booking/model/dto/RecruiterDTO.java
@@ -1,4 +1,13 @@
 package com.soprasteria.booking.model.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class RecruiterDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String initials;
 }
