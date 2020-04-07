680f30ad03c6ae1e0114bc29318222ae62b5ac92
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-04-07 13:08:19 +0200

    feat: added author to README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+#API-REST Sopra Booking
 #Hal-Explorer
 http://localhost:8080/explorer/index.html#theme=Darkly&uri=/
-
+#author
+https://github.com/BladimirDelBarrioPizarro
--- src/test/java/com/soprasteria/booking/service/RecruiterServiceTestImpl.java
+++ src/test/java/com/soprasteria/booking/service/RecruiterServiceTestImpl.java
@@ -6,7 +6,6 @@ import com.soprasteria.booking.service.dummy.RecruiterServiceDummy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
