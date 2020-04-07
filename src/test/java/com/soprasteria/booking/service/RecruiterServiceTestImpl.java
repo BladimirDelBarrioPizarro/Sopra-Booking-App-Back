7aa38e4b420797613a3d8d63a8c1ed51db2bb3de
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-04-07 12:33:59 +0200

    feat: added test findAllTest in RecruiterServiceTestImpl

--- src/test/java/com/soprasteria/booking/service/RecruiterServiceTestImpl.java
+++ src/test/java/com/soprasteria/booking/service/RecruiterServiceTestImpl.java
@@ -1,8 +1,27 @@
 package com.soprasteria.booking.service;
 
+import com.soprasteria.booking.model.dto.RecruiterDTO;
+import com.soprasteria.booking.service.dummy.RecruiterServiceDummy;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class RecruiterServiceTestImpl {
+    private RecruiterService recruiterService = mock(RecruiterService.class);
+
+
+    @Test
+    void findAllTest(){
+        List<RecruiterDTO> recruiterDTOList = RecruiterServiceDummy.recruiterDTOListDummy();
+        when(recruiterService.findAll()).thenReturn(recruiterDTOList);
+        List<RecruiterDTO> recruiterDTOList2 = recruiterService.findAll();
+        assert (recruiterDTOList2.size() > 0);
+    }
+
 }
